d7c503c5bad30893fb6d1dabdb938102e55e5c72
Trung Hieu Le <12520839@gm.uit.edu.vn>
2016-04-05 13:29:41 +0700

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,7 @@ import media
 import fresh_tomatoes
 
 
-#take the instances of class Movie about my favorite movies
+#Take the instances of class Movie about my favorite movies
 toy_story = media.Movie("Toy Story",
                         "http:/upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://youtube.com/watch?v=vwyZH85NQC4")
@@ -27,8 +27,8 @@ three_Idiots = media.Movie("3 Idiots",
                            "https://goo.gl/W0css6",
                            "https://www.youtube.com/watch?v=xvszmNXdM4w")
 
-#list name of my favorite movies
+#List name of my favorite movies
 movies = [toy_story, titanic, harry_potter, furious, arrow, three_Idiots]
 
-#open Movies Trailer page
+#Open Movies Trailer page
 fresh_tomatoes.open_movies_page(movies)
