669786bddf5585f40f7f6d54c0f44b3abe5adb31
Trung Hieu Le <12520839@gm.uit.edu.vn>
2016-04-05 14:30:52 +0700

    Update media.py

--- media.py
+++ media.py
@@ -2,24 +2,22 @@ import webbrowser
 
 
 class Movie:
-    # Introduction about class Movie
     """ This class provides a way to store movie related information and show trailer
     Attributes:
         title (str): This saves title of movie.
         poster_image_url (str): This saves a url linking to poster image of movie.
         trailer_youtube_url (str): This saves a url linking to trailer of movie.
     Method:
+        __init__()    : This init a instance of class Movie.
         show_trailer(): this helps to show trailer of movie.
     """
 
 
-    # Init a insance of class Movie
     def __init__(self, movie_title, post_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = post_image
         self.trailer_youtube_url =  trailer_youtube
 
 
-    # Open web browser and show trailer.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
